a37e623564a62d6f3c4a107e34b16683cb2dcb67
Denys_Basun <denbasxl@gmail.com>
2024-08-12 14:19:52 +0300

    change url and http.hook.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ import {filtersFetching, filtersFetched, filtersFetchingError} from '../componen
 export const fetchHeroes = (request) => (dispatch) =>{
     dispatch(heroesFetching());
 
-        request("https://vercel-api-github-io.vercel.app/api/data")
+        request("https://json-server-github-io.vercel.app/heroes")
             .then(data => dispatch(heroesFetched(data.heroes)))
             .catch(() => dispatch(heroesFetchingError()))
 }
@@ -12,7 +12,7 @@ export const fetchHeroes = (request) => (dispatch) =>{
 export const fetchFilters = (request) => (dispatch) =>{
     dispatch(filtersFetching());
 
-        request("https://vercel-api-github-io.vercel.app/api/data")
+        request("https://json-server-github-io.vercel.app/filters")
             .then(data => dispatch(filtersFetched(data.filters)))
             .catch(() => dispatch(filtersFetchingError()))
 }
--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 export const useHttp = () => {
-    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json', 'Cache-Control': 'no-cache'}) => {
         try {
             const response = await fetch(url, {method, body, headers});
 
