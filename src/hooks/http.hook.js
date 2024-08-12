af8efad7e5d7a36a0ea0dbbb642161a085f5a584
Denys_Basun <denbasxl@gmail.com>
2024-08-12 14:22:43 +0300

    change hook

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
 
 export const useHttp = () => {
-    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json', 'Cache-Control': 'no-cache'}) => {
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
         try {
             const response = await fetch(url, {method, body, headers});
 
