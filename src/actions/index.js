c43d19a0d05604cbed1000e05607c64e32dcad7e
Denys_Basun <denbasxl@gmail.com>
2024-08-12 14:06:13 +0300

    change data

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,16 +4,16 @@ import {filtersFetching, filtersFetched, filtersFetchingError} from '../componen
 export const fetchHeroes = (request) => (dispatch) =>{
     dispatch(heroesFetching());
 
-        request("https://vercel-api-github-io.vercel.app/api/data/heroes")
-            .then(data => dispatch(heroesFetched(data)))
+        request("https://vercel-api-github-io.vercel.app/api/data")
+            .then(data => dispatch(heroesFetched(data.heroes)))
             .catch(() => dispatch(heroesFetchingError()))
 }
 
 export const fetchFilters = (request) => (dispatch) =>{
     dispatch(filtersFetching());
 
-        request("https://vercel-api-github-io.vercel.app/api/data/filters")
-            .then(data => dispatch(filtersFetched(data)))
+        request("https://vercel-api-github-io.vercel.app/api/data")
+            .then(data => dispatch(filtersFetched(data.filters)))
             .catch(() => dispatch(filtersFetchingError()))
 }
 
